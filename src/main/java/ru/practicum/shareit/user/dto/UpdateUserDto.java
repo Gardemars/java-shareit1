7bc7a1ade643bc037d1b08d0393c4c35df0c5a6a
Gardemars <kugoev@yandex.ru>
2023-08-21 00:28:43 +0300

    Sprint № 14, код-ревью № 1

---
+++ src/main/java/ru/practicum/shareit/user/dto/UpdateUserDto.java
@@ -0,0 +1,12 @@
+package ru.practicum.shareit.user.dto;
+
+import lombok.Value;
+
+import javax.validation.constraints.Email;
+
+@Value
+public class UpdateUserDto {
+    String name;
+    @Email(message = "Введите правильный email")
+    String email;
+}
